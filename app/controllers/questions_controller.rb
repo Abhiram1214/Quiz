d2029c31410e61e623f0d6d4e7b485b9f07704ab
Abhiram Challapalli <abhiram_cvns@hotmail.com>
2015-03-17 18:27:43 +0530

    prev and next working

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,19 +2,49 @@ class QuestionsController < ApplicationController
   def index
 
     @questions=Question.first
-    #@nextquestion=@questions.
+      p params[:counter]
+        #@nextquestion=@questions.
         #@next=@questions.i
   end
 
   def next
-    @questions=Question.find(params[:count])
-    @iterate=@questions.id.next
-    @next=Question.find_by(:id=>@iterate)
-
-    #p @iterate
 
     respond_to do |format|
       format.js{}
+    end
+  end
+
+  def prev
+
+     respond_to do |format|
+       format.js{}
+     end
+  end
+
+  def allquestions
+    @questions=Question.find(params[:count])
+
+       respond_to do |format|
+         format.js{}
+       end
+    #@questions=Question.find(params[:count])
+    #@iterate=@questions.id.next
+    #@next=Question.find_by(:id=>@iterate)
+
+  end
+
+  def prevquestions
+    @questions=Question.find(params[:negcount])
+
+    respond_to do |format|
+      format.js{ }
+    end
+  end
+
+  def buttonvalue
+     #@buttonvalue=Answer.find(params[:id])
+     respond_to do |format|
+       format.js{"render :nothing=>true"}
     end
   end
 end
